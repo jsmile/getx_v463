3283711a4446af54228fca0b10d1ba299a5160af
jsmile <jsmile@naver.com>
2024-08-07 22:52:45 +0900

    basic Get.create<ControllerT>(...)

--- lib/dependency_mamagement/detail_page.dart
+++ lib/dependency_mamagement/detail_page.dart
@@ -9,8 +9,9 @@ class DetailPage extends StatelessWidget {
   DetailPage({super.key});
 
   // 호출하는 widget 으로부터 전달받은 Controller 구하기
-  final controller = Get.find<Controller>();
-  // Controller controller = Get.find<Controller>();
+  // final controller = Get.find<Controller>();
+  Controller controller = Get.find();
+  final controller2 = Get.find<Controller>();
   // final child = Get.find<Parent>();
 
   @override
@@ -31,13 +32,19 @@ class DetailPage extends StatelessWidget {
                 style: const TextStyle(fontSize: 30),
               ),
             ),
+            Obx(
+              () => Text(
+                '${controller2.count.value}', // 수정이 아닌 조회 시에는 .value 사용
+                style: const TextStyle(fontSize: 30),
+              ),
+            ),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () => Get.find<Controller>().increment(),
+        onPressed: () => controller.increment(),
+        // onPressed: () => Get.find<Controller>().increment(),
         // onPressed: () => Get.find<Controller>(tag: 'uniqueId').increment(),
-        // onPressed: () => controller.increment(),
         child: const Icon(Icons.add),
       ),
     );
--- lib/dependency_mamagement/view.dart
+++ lib/dependency_mamagement/view.dart
@@ -41,8 +41,8 @@ class DepnedencyView extends StatelessWidget {
           // 호출하는 Wiget 에 Controller 전달을 위한 binding
           // binding: DetailsBinding(),
           binding: BindingsBuilder(() {
-            // LazyLoad a dependency only when it is used.
-            Get.lazyPut<Controller>(() => Controller());
+            // // LazyLoad a dependency only when it is used.
+            // Get.lazyPut<Controller>(() => Controller());
             // Get.lazyPut<Controller>(
             //   // method will be excuted when your class
             //   () => Controller(),
@@ -52,15 +52,30 @@ class DepnedencyView extends StatelessWidget {
             //   // delete() 시에도 메모리에 유지시켰다가 재 호출 시 빠른 생성여부 결정.
             //   fenix: false,
             // );
-            Get.putAsync<AsyncTask>(
-              () async {
-                await Future.delayed(const Duration(seconds: 3)); // 3초 대기
-                return AsyncTask(); // async 작업 수행
-              },
-              // using Get.find<Controller>() with unique id
-              tag: 'uniqueId for putAsync',
-              // kept instance throughout the entire app.
-              permanent: false,
+
+            // Get.putAsync<AsyncTask>(
+            //   () async {
+            //     await Future.delayed(const Duration(seconds: 3)); // 3초 대기
+            //     return AsyncTask(); // async 작업 수행
+            //   },
+            //   // using Get.find<Controller>() with unique id
+            //   tag: 'uniqueId for putAsync',
+            //   // kept instance throughout the entire app.
+            //   permanent: false,
+            // );
+
+            // Create new instance every time, not Singleton instance
+            Get.create<Controller>(
+              // a function that returns a class
+              () => Controller(),
+              // tag: 'uniqueId for create',
+              permanent: true,
+            );
+            Get.create<Controller>(
+              // a function that returns a class
+              () => Controller(),
+              // tag: 'uniqueId for create',
+              permanent: true,
             );
           }),
         ),
